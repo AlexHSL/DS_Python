9715c584c6e8a4cf14f71ac741520fa22547fbdc
AlexHSL <2825883930@qq.com>
2020-08-29 11:01:19 +0800

    hello python interpreter!

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,18 @@
-print("hello world!")
-print("test")
+message = "hello world!"
+
+print(message.title())
+print(message.upper())
+
+message="A"+message
+print(message.lower())
+
+message = "  " + message + " "
+print(message.strip()+"end")
+print(message.lstrip()+"end")
+print(message.rstrip()+"end")
+print(message+"end")
+
+test_string = "test\n\t"
+print(test_string+"end")
+print(test_string.strip()+"end")
+
